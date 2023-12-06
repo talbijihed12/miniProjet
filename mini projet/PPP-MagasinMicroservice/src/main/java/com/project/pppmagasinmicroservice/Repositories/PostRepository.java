3e46aa21727e57047eacb34fc4feaca5aa2d7664
Talbi Jihed <talbijihed302@gmail.com>
2023-12-06 18:12:01 +0100

    first commit

---
+++ mini projet/PPP-MagasinMicroservice/src/main/java/com/project/pppmagasinmicroservice/Repositories/PostRepository.java
@@ -0,0 +1,17 @@
+package com.project.pppmagasinmicroservice.Repositories;
+
+import com.project.pppmagasinmicroservice.Entity.Post;
+import com.project.pppmagasinmicroservice.Entity.Topic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByTopic(Topic topic);
+
+
+
+
+
+}
